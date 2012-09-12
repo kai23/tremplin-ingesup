5fbf61dc0a0418b778d825565c703dbd4cdb53c2
cactusmutant <cactus@localhost.localdomain>
2012-09-12 11:55:03 +0200

    Exercice while avec sortie

---
+++ tp_boucle_while.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+temp=""
+while read entree
+do
+    if [[ "$entree" == "q" ]]
+    then
+        break
+    fi
+    echo "Saisie : $entree"
+    temp="$temp\n$entree"
+done
+
+echo "Terminaison normale"
+echo -e "Vous avez tapé $temp"
